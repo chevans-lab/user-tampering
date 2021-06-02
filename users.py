df329afbf3fec57c8a55108213cd1ef5af6c2189
Charles Evans <charles.evans12321@gmail.com>
2021-06-02 21:28:19 +1000

    Defined unseen user population

--- users.py
+++ users.py
@@ -24,11 +24,20 @@ class Users:
 
     def __init__(self):
         self.users = {}
+        # Main User Population for training and evaluation
         self.users["Moderate Right"] = np.array([0.1, 0.25, 0.3])
         self.users["Strong Right"] = np.array([0.1, 0.1, 0.4])
         self.users["Moderate Left"] = np.array([0.3, 0.25, 0.1])
         self.users["Strong Left"] = np.array([0.4, 0.1, 0.1])
         self.users["Centrist"] = np.array([0.2, 0.4, 0.2])
+
+        # BELOW: Additional users which are not used in training, but can be uncommented for evaluation to
+        # verify that the learned policy is robust to preference profiles which were not encountered in training.
+
+        #self.users["Extremely Right"] = np.array([0.05, 0.05, 0.5])
+        #self.users["Right anti-centrist "] = np.array([0.2, 0.05, 0.35])
+        #self.users["Left anti-centrist"] = np.array([0.35, 0.05, 0.2])
+        #self.users["Extremely Left"] = np.array([0.5, 0.05, 0.05])
 
     def get_users(self):
         return self.users
